f2229ecee531dfe2a8b1cceb9aa094ef8b42ba76
Zach Gannon <7028393+NonnagHcaz@users.noreply.github.com>
2025-01-23 18:38:33 -0500

    Reorganized code. Added tryUntilLive args. Created constants from hardcoded values.

--- js/modules/channel.js
+++ js/modules/channel.js
@@ -1,15 +1,29 @@
-import { STORAGE_CHANNELS_KEY } from "./constants.js";
+import { STORAGE_CHANNELS_KEY, TWITCH_DEFAULT_MAX_TRIES, TWITCH_DEFAULT_RETRY_DELAY, TWITCH_STREAM_ISLIVE_TEXT, TWITCH_STREAM_ISLIVE_XPATH, TWITCH_STREAM_TITLE_ATTRIBUTE, TWITCH_STREAM_TITLE_XPATH } from "./constants.js";
 import { getElementByXpath } from "./dom.js";
 import { readLocalStorage, writeLocalStorage } from "./storage.js";
 import { isWatchingChannel } from "./tab.js";
 
+// -------------------------------------------------------------------------------------
+// Twitch DOM parsing functions
+// -------------------------------------------------------------------------------------
 
+async function getTwitchResponse(channel, maxTries=TWITCH_DEFAULT_MAX_TRIES, retryDelay=TWITCH_DEFAULT_RETRY_DELAY, tryUntilLive=false) {
+    return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage({contentScriptQuery: "getTwitchResponse", channel: channel, maxTries: maxTries, retryDelay: retryDelay, tryUntilLive: tryUntilLive}, function(response) {
+            if (response) {
+                resolve(response.text);
+            } else {
+                reject(new Error("Failed to get Twitch response"));
+            }
+        });
+    });
+}
 
-async function getChannelData(channel, maxTries=3, retryDelay=100) {
-    const text = await getTwitchResponse(channel, maxTries, retryDelay);
+async function getChannelData(channel, maxTries=TWITCH_DEFAULT_MAX_TRIES, retryDelay=TWITCH_DEFAULT_RETRY_DELAY, tryUntilLive=false) {
+    const text = await getTwitchResponse(channel, maxTries, retryDelay, tryUntilLive);
     const doc = new DOMParser().parseFromString(text, "text/html");
     const isWatching = await isWatchingChannel(channel);
-    const isLive = _isLive(doc);
+    const isLive = _isChannelLiveFromResponse(doc);
     var data = { channel, isLive, isWatching, streamTitle: null };
     if (isLive) {
         const streamTitle = _getStreamTitleFromResponse(doc);
@@ -22,53 +36,39 @@ async function getChannelData(channel, maxTries=3, retryDelay=100) {
     return data;
 }
 
-async function getTwitchResponse(channel, maxTries=3, retryDelay=100) {
-    return new Promise((resolve, reject) => {
-        chrome.runtime.sendMessage({contentScriptQuery: "getTwitchResponse", channel: channel, maxTries: maxTries, retryDelay: retryDelay, tryUntilLive: true}, function(response) {
-            if (response) {
-                resolve(response.text);
-            } else {
-                reject(new Error("Failed to get Twitch response"));
-            }
-        });
-    });
+async function getStreamTitle(channel, maxTries=TWITCH_DEFAULT_MAX_TRIES, retryDelay=TWITCH_DEFAULT_RETRY_DELAY, tryUntilLive=false) {
+    const text = await getTwitchResponse(channel, maxTries, retryDelay, tryUntilLive);
+    const doc = new DOMParser().parseFromString(text, "text/html");
+    return _getStreamTitleFromResponse(doc);
 }
 
-async function getStreamTitle(channel, maxTries=3, retryDelay=100) {
-    const text = await getTwitchResponse(channel, maxTries, retryDelay);
-    return _getStreamTitleFromResponse(text);
+function _getStreamTitleFromResponse(doc) {
+    // Parse the meta:description tag from the response
+    const element = getElementByXpath(TWITCH_STREAM_TITLE_XPATH, doc);
+    return element ? element.getAttribute(TWITCH_STREAM_TITLE_ATTRIBUTE) : null;
 }
 
-
-
-async function isChannelLive(channel, maxTries=3, retryDelay=100) {
-    const text = await getTwitchResponse(channel, maxTries, retryDelay);
-    return _isLive(text);
+async function isChannelLive(channel, maxTries=TWITCH_DEFAULT_MAX_TRIES, retryDelay=TWITCH_DEFAULT_RETRY_DELAY, tryUntilLive=false) {
+    const text = await getTwitchResponse(channel, maxTries, retryDelay, tryUntilLive);
+    const doc = new DOMParser().parseFromString(text, "text/html");
+    return _isChannelLiveFromResponse(doc);
 }
 
+function _isChannelLiveFromResponse(doc) {
+    const element = getElementByXpath(TWITCH_STREAM_ISLIVE_XPATH, doc);
+    return element && element.textContent.includes(TWITCH_STREAM_ISLIVE_TEXT);
+}
+
+// -------------------------------------------------------------------------------------
+// Storage functions
+// -------------------------------------------------------------------------------------
 
 async function getStoredChannels() {
     return await readLocalStorage(STORAGE_CHANNELS_KEY);
 }
 
 async function addChannelToStorage(channel) {
-    var channels = await getStoredChannels();
-    if (!channels.includes(channel)) {
-        channels.push(channel);
-        await writeLocalStorage(STORAGE_CHANNELS_KEY, channels);
-        return true;
-    }
-    return false;
-}
-
-async function removeChannelFromStorage(channel) {
-    var channels = await getStoredChannels();
-    if (channels.includes(channel)) {
-        channels = channels.filter((c) => c !== channel);
-        await writeLocalStorage(STORAGE_CHANNELS_KEY, channels);
-        return true;
-    }
-    return false;
+    return await addChannelsToStorage([channel]);
 }
 
 async function addChannelsToStorage(channels) {
@@ -82,6 +82,10 @@ async function addChannelsToStorage(channels) {
     await writeLocalStorage(STORAGE_CHANNELS_KEY, storedChannels);
 }
 
+async function removeChannelFromStorage(channel) {
+    return await removeChannelsFromStorage([channel]);
+}
+
 async function removeChannelsFromStorage(channels) {
     var storedChannels = await getStoredChannels();
     for (var i = 0; i < channels.length; i++) {
@@ -91,17 +95,6 @@ async function removeChannelsFromStorage(channels) {
     await writeLocalStorage(STORAGE_CHANNELS_KEY, storedChannels);
 }
 
-function _getStreamTitleFromResponse(doc) {
-    // Parse the meta:description tag from the response
-    const xpath = "//meta[contains(@name, 'description')]";
-    const metaDescription = getElementByXpath(xpath, doc);
-    return metaDescription ? metaDescription.getAttribute("content") : null;
-}
-
-function _isLive(doc) {
-    var text = doc.head.innerHTML;
-    return text.includes("isLiveBroadcast");
-}
 
 export { addChannelsToStorage, addChannelToStorage, getChannelData, getStoredChannels, getStreamTitle, isChannelLive, removeChannelFromStorage, removeChannelsFromStorage };
 
